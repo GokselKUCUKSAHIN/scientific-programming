9f9b43001b91fd58e8bd901eebeeb8223c441033
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2021-01-28 04:17:22 +0300

    Typo fixed and comments added.

--- Workspace/CTE/ImageProcessing/W1/intermediate.m
+++ Workspace/CTE/ImageProcessing/W1/intermediate.m
@@ -1,16 +1,24 @@
 close all;
+
+% load image from file
 im = imread('basicsImg/faculty.jpg');
 figure; imshow(im);
+
+% open image wiith imtool 
 imtool(im);
 
+%flip image left-right
 imlr = fliplr(im);
+
+%flip image upside-down
 imud = flipud(im);
 
 figure; imshow(imlr); title('Mirror left-right');
 figure; imshow(imud); title('Mirror upside-down');
 
+% Convert Color image to Gray Scale
 imgray = rgb2gray(im); 
 figure; imshow(imgray); title('Gray SCALE');
-imgray2 = im2gray(im);
+imgray2 = im2gray(im); % same as rgb2gray but results are same
 
-disp(isequal(imgray, imgray2));
+disp(isequal(imgray, imgray2)); % Proof to same
